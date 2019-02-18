c8387ff56a2a867f0affb4a3f3e4f30e910f257a
Gabriel Stahlberg Oliveira <gabriel.oliveira@shx0313-linux.shxinfor.local>
2019-02-18 17:27:32 -0300

    Criação do DAO, métodos de adição, listagem e remoção.

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -20,6 +20,18 @@ class NegociacaoController {
       new MensagemView($('#mensagemView')),
       'texto'
     );
+
+    connectionFactory
+      .getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.listaTodos())
+      .then(negociacoes => 
+          negociacoes.forEach(negociacao => 
+            this._listaNegociacoes.adiciona(negociacao)))
+      .catch(erro => {
+        console.log(erro);
+        this._mensagem.texto = erro;
+      })
   }
 
   importaNegociacoes() {
@@ -40,14 +52,35 @@ class NegociacaoController {
 
   adiciona(event) {
     event.preventDefault();
-    this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._mensagem.texto = 'Negociação adicionada com sucesso';
-    this._limpaFormulario();
+
+    connectionFactory
+      .getConnection()
+      .then(connection => {
+
+        let negociacao = this._criaNegociacao();
+
+        new NegociacaoDao(connection)
+          .adiciona(negociacao)
+          .then(() => {
+            this._listaNegociacoes.adiciona(negociacao);
+            this._mensagem.texto = 'Negociação adicionada com sucesso';
+            this._limpaFormulario();
+          })
+      })
+      .catch(erro => this._mensagem.texto = erro);
   }
 
   apaga() {
-    this._listaNegociacoes.esvazia();
-    this._mensagem.texto = 'Lista de negociações apagada com sucesso.';
+    connectionFactory
+      .getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.apagaTodos())
+      .then(mensagem => {
+        this._mensagem.texto = mensagem;
+        this._listaNegociacoes.esvazia();
+      })
+      .catch(erro => this._mensagem.texto = erro);
+
   }
 
   ordena(coluna) {
@@ -62,8 +95,8 @@ class NegociacaoController {
   _criaNegociacao() {
     return new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
-      this._inputQuantidade.value,
-      this._inputValor.value
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
     );
   }
 
